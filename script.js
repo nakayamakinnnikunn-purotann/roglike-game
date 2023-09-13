a21d449c21c9b62ab1b05498725d4488de4c21fc
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-09-13 10:31:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-        // Canvas要素を取得
+  // Canvas要素を取得
         var canvas = document.getElementById("myCanvas");
         var context = canvas.getContext("2d");
 
-        // キャンバスの幅と高さを取得
-        var canvasWidth = canvas.width;
-        var canvasHeight = canvas.height;
+        // キャンバスのサイズをウィンドウサイズに合わせる
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-        // 5本の縦線を引く
-        for (var i = 1; i <= 5; i++) {
-            var x = (canvasWidth / 5) * i;
-            context.beginPath();
-            context.moveTo(x, 0);
-            context.lineTo(x, canvasHeight);
-            context.stroke();
+        // ウィンドウのリサイズ時にCanvasのサイズを調整する
+        window.addEventListener("resize", function () {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            drawLines();
+        });
+
+        // 縦に5本の線を描画
+        function drawLines() {
+            var lineWidth = canvas.width / 5;
+
+            for (var i = 1; i < 5; i++) {
+                var x = lineWidth * i;
+                context.beginPath();
+                context.moveTo(x, 0);
+                context.lineTo(x, canvas.height);
+                context.stroke();
+            }
         }
+
+        // 初回描画
+        drawLines();
