170efd697a7b788a4858c6e0d4c12088e4623346
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-09-13 12:17:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,29 @@
 
         // 初回描画
         drawLines();
+var drawHorizontalLineAnim = function() {
+  var beginPos = 0,  // スタート位置
+      movePos  = beginPos,  // 移動位置（現在位置）
+      addVal   = 10,  // 加算量
+      endPos   = csWidth - 10,  // 終了位置
+      isAnim   = function() {  // アニメーションを終了する条件
+        return (movePos < endPos);
+      };
+
+  var render = function() {
+    ctx.beginPath();
+    ctx.moveTo(beginPos, center.y);
+    ctx.lineTo(movePos, center.y);
+    ctx.closePath();
+    ctx.stroke();
+
+    if (isAnim() === true) {
+      movePos += addVal;
+      // ↑のaddで終了点を超えることがあるため上限を決める
+      movePos = (isAnim() === false) ? endPos : movePos;
+      requestAnimationFrame(render)
+    }
+  };
+  render();
+};
+drawHorizontalLineAnim();
